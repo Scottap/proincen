dd5b5b52f169282d2d2bfd88d7d2bb3a7538bb78
Flavio Ruiz <fsruiz0@gmail.com>
2018-04-30 10:11:46 -0400

    agregado menu movil, arreglados iconos en movil

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,12 @@ $(document).ready(function(){
         loop: true
     });
 });
+
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
